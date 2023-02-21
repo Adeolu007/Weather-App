83d16dd8f64d37cb73a106e9975e97bfc34f1226
Adeolu Odunuyi <93635641+Adeolu007@users.noreply.github.com>
2023-02-21 00:17:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -30,5 +30,10 @@ let weather = {
 document.querySelector(".search button").addEventListener("click", function (){
  weather.search()
 })
+document.querySelector(".search-bar").addEventListener("keyup", function (e){
+ if (e.key == "Enter"){
+    weather.search()
+ }
+   })
 
 
